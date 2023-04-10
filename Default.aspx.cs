195ed653bfcbc83453890fe6bedf7f16ff923bed
Abul Hasnat Mahraz <93318140+mahraz-hasnat@users.noreply.github.com>
2023-04-10 09:49:30 +0600

    Update Default.aspx.cs

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,102 +1,85 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace AspWebapp2
+namespace WebApplication2
 {
     public partial class _Default : Page
     {
-        string connectionString = @"Data Source=YourServerName;Initial Catalog=YourDatabaseName;User ID=YourUserName;Password=YourPassword;";
-        string select_query = @"SELECT s.name as [Name] , cr.course_name as [Courese Name], cr.course_instructor as [Instructor], cr.credit as [Course Credit], s.about as [About Student] FROM student s JOIN course cr ON s.course_id = cr.ID;";
-        string insert_query = @"INSERT INTO student (ID, name, age, about, course_id) VALUES (@ID, @Name, @Age, @About, @Course_id);";
-        string update_query = @"";
-        string delete_query = @"";
-        string insert_course_query = @"INSERT INTO course (ID, course_name, course_instructor, credit) VALUES (@ID, @Course_name, @Instructor, @Credit);";
+        string connectionString = @"Data Source=127.0.0.1;Initial Catalog=test-mh;User ID=sa;Password=Ahal1234!;";
+        string select_query = @"SELECT s.ID,  s.name as [Name] , cr.course_name as [Courese Name], cr.course_instructor as [Instructor], cr.credit as [Course Credit], s.about as [About Student] FROM student s JOIN course cr ON s.course_id = cr.ID;";
+        //string insert_query = @"INSERT INTO student (ID, name, age, about, course_id) VALUES ('"+int.Parse(TextBox1.Text)+"', '"+TextBox2.Text+"', '"+TextBox3.Text+"', '"+TextBox4.Text+"', '"+int.Parse(DropDownList1.SelectedValue)+"');";
+        //string update_query = @"";
+        //string delete_query = @"";
+        //string insert_course_query = @"INSERT INTO course (ID, course_name, course_instructor, credit) VALUES (, '"+int.Parse(TextBox2.Text)+"', '"+int.Parse(TextBox3.Text)+"','"+int.Parse(DropDownList1.SelectedValue)+"';";
 
         protected void Page_Load(object sender, EventArgs e)
         {
-
-        }
-
-        protected void show_Click(object sender, EventArgs e)
-        {
-            SqlConnection connection = new SqlConnection(connectionString);
-            SqlCommand command = new SqlCommand(select_query, connection);
-            
-            connection.Open();
-            SqlDataReader reader = command.ExecuteReader();
-            GridView1.DataSource = reader;
-            GridView1.DataBind();
-            connection.Close();
-        }
-
-        protected void insert_Click(object sender, EventArgs e)
-        {
-            if (IsPostBack)
+            if (!IsPostBack)
             {
-                int id = int.Parse(Request.Form["id"]);
-                string name = Request.Form["name"];
-                int age = int.Parse(Request.Form["age"]);
-                string about = Request.Form["about"];
-                int course_id = int.Parse(Request.Form["id0"]);
-
-                SqlConnection connection = new SqlConnection(connectionString);
+                //loadData();
+                BindGrid();
                 
-                SqlCommand command = new SqlCommand(insert_query, connection);
-                connection.Open();
-                command.Parameters.AddWithValue("@ID", id);
-                command.Parameters.AddWithValue("@Name", name);
-                command.Parameters.AddWithValue("@Age", age);
-                command.Parameters.AddWithValue("@About", about);
-                command.Parameters.AddWithValue("@Course_id", course_id);
-                int rowsAffected = command.ExecuteNonQuery();
-                connection.Close();
             }
         }
 
-        protected void update_Click(object sender, EventArgs e)
+        protected void BindGrid()
         {
-            SqlConnection connection = new SqlConnection(connectionString);
-
-            SqlCommand command = new SqlCommand(update_query, connection);
-            connection.Open();
-
-            connection.Close();
+            Class1 load = new Class1();
+            DataTable dt = load.LoadData();
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
         }
 
-        protected void delete_Click(object sender, EventArgs e)
+        protected void InsertStudent(object sender, EventArgs e)
         {
-            SqlConnection connection = new SqlConnection(connectionString);
+            int id = int.Parse(Request.Form["id"]);
+            string name = Request.Form["name"];
+            int age = int.Parse(Request.Form["age"]);
+            string about = Request.Form["about"];
+            int course_id = int.Parse(Request.Form["crs_id"]);
 
-            SqlCommand command = new SqlCommand(delete_query, connection);
-            connection.Open();
-            connection.Close();
+            Class1  save = new Class1();
+            save.SaveData(id,name,age,about,course_id);
+            
         }
 
-        protected void insert_course_Click(object sender, EventArgs e)
+        protected void UpdateStudent(object sender, EventArgs e)
         {
-            if (IsPostBack)
-            {
-                int cid = int.Parse(Request.Form["cid"]);
-                string cname = Request.Form["cname"];
-                string instructor = Request.Form["instructor"];
-                int credit = int.Parse(Request.Form["credit"]);
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand(@"UPDATE student set name = '"+ TextBox2.Text +"', age = '" + TextBox3.Text + "', about = '" + TextBox4.Text + "', course_id = '" + int.Parse(DropDownList1.SelectedValue) + "' WHERE ID = '" + int.Parse(TextBox1.Text) + "';", conn);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            BindGrid();
+        }
 
-                SqlConnection connection = new SqlConnection(connectionString);
+        protected void DeleteStudent(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand(@"Delete from student WHERE ID = '" + int.Parse(TextBox1.Text) + "';", conn);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            BindGrid();
+        }
 
-                SqlCommand command = new SqlCommand(insert_course_query, connection);
-                connection.Open();
-                command.Parameters.AddWithValue("@ID", cid);
-                command.Parameters.AddWithValue("@Course_name", cname);
-                command.Parameters.AddWithValue("@Instructor", instructor);
-                command.Parameters.AddWithValue("@Credit", credit);
-                int rowsAffected = command.ExecuteNonQuery();
-                connection.Close();
-            }
-        } 
+        //protected void loadData()
+        //{
+        //    SqlConnection conn = new SqlConnection(connectionString);
+        //    SqlCommand cmd = new SqlCommand(select_query, conn);
+        //    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+        //    DataTable dt = new DataTable();
+        //    //conn.Open();
+        //    adapter.Fill(dt);
+        //    //conn.Close();
+        //    GridView1.DataSource = dt;
+        //    GridView1.DataBind();
+        //}
     }
 }
